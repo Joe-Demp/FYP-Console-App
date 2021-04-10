d6b93b9557a198e749c6fad1cded78447a73a08c
Joseph Dempsey <joseph.dempsey@ucdconnect.ie>
2021-04-10 19:25:49 +0100

    Removed the test code from main, adapted it into run, refactored run

--- src/main/java/ie/ucd/dempsey/Main.java
+++ src/main/java/ie/ucd/dempsey/Main.java
@@ -12,13 +12,11 @@ import service.core.Constants;
 
 import java.net.InetSocketAddress;
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class Main implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -29,68 +27,64 @@ public class Main implements Runnable {
     @Option(names = {"-o", "--orchestrator"}, defaultValue = "ws://csi420-01-vm1.ucd.ie",
             paramLabel = "Orchestrator URI", description = "Should be of the form: ws://{host}[:{port}]")
     private URI orchestratorUri;
+    private PingServer pingServer;
+    private Thread pingThread;
+    private CommandLineWsClient wsClient;
+    private ExecutorService httpClientExecutor = Executors.newSingleThreadExecutor();
+    private ScheduledExecutorService clHttpClientExecutor = Executors.newSingleThreadScheduledExecutor();
 
     public static void main(String[] args) {
         // take in an Orchestrator address, return the status code
-//        System.exit(new CommandLine(new Main()).execute(args));
-
-        // todo remove this test section
-        AtomicReference<URI> uriRef = new AtomicReference<>();
-        ExecutorService exec = Executors.newSingleThreadExecutor();
-        HttpClient httpClient = HttpClient.newBuilder().executor(exec).build();
-        CommandLineHttpClient client = new CommandLineHttpClient(uriRef, httpClient);
-        String getResult = null;
-
-        assert uriRef.get() == null;
-        try {
-            getResult = client.getData();
-        } catch (RuntimeException re) {
-            logger.error("Error getting data with null reference");
-            logger.debug("getResult={}", getResult);
-        }
-
-        uriRef.set(URI.create("http://localhost:8080"));
-//        URI uri = uriRef.get();
-//        logger.debug(uri.toString());
-//        logger.debug(uri.getScheme());
-//        logger.debug(uri.getHost());
-//        logger.debug(String.valueOf(uri.getPort()));
-//        logger.debug(uri.getPath());
-//        try {
-//            logger.debug(new URL(uri.getScheme(), uri.getHost(), uri.getPort(), "/").toString());
-//        } catch (Exception e) {}
-        getResult = client.getData();
-        logger.debug("getResult={}", getResult);
-
-        client.run();
-
-        getResult = client.getData();
-        logger.debug("getResult={}", getResult);
-
-        exec.shutdownNow();
+        System.exit(new CommandLine(new Main()).execute(args));
     }
-
 
     @Override
     public void run() {
         // take in an address
         logger.debug("command line orchestrator address: " + orchestratorUri);
+        startPingServer();
+        startWebSocketClient();
+        startHttpClient();
+        startApplication();
+        shutdownEverything();
+        logger.debug("Finished.");
+    }
 
-        PingServer pingServer = new PingServer(new InetSocketAddress(Constants.MOBILE_PING_SERVER_PORT));
-        Thread pingThread = new Thread(pingServer);
-        pingThread.start();
-
-        CommandLineWsClient wsClient = new CommandLineWsClient(orchestratorUri);
+    private void startWebSocketClient() {
+        wsClient = new CommandLineWsClient(orchestratorUri);
         wsClient.connect();
+    }
 
-        // start the Http data process
+    private void startPingServer() {
+        pingServer = new PingServer(new InetSocketAddress(Constants.MOBILE_PING_SERVER_PORT));
+        pingThread = new Thread(pingServer);
+        pingThread.start();
+    }
+
+    // schedules the CommandLineHttpClient task, that sends data to cloud services
+    private void startHttpClient() {
+        HttpClient httpClient = HttpClient.newBuilder().executor(httpClientExecutor).build();
         ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
-        exec.scheduleAtFixedRate(new CommandLineHttpClient(wsClient.getCloudServiceReference(), null),
-                2, 2, TimeUnit.SECONDS);
+        clHttpClientExecutor.scheduleAtFixedRate(
+                new CommandLineHttpClient(wsClient.getCloudServiceReference(), httpClient),
+                2, 2, TimeUnit.SECONDS
+        );
+    }
 
-        // start the application
+    private void startApplication() {
         CommandLineApplication app = new CommandLineApplication(wsClient);
         app.start();
+    }
+
+    /*
+     * stops the executor that the CommandLineHttpClient's HttpClient uses. Causes a delay and error with mvn exec if
+     * this is not shutdown manually.
+     * Then stops the scheduled CommandLineHttpClient task.
+     * Then shuts down the PingServer.
+     */
+    private void shutdownEverything() {
+        httpClientExecutor.shutdown();
+        clHttpClientExecutor.shutdown();
 
         try {
             final int timeout = 3 * 1000;
@@ -99,6 +93,5 @@ public class Main implements Runnable {
         } catch (InterruptedException ie) {
             ie.printStackTrace();
         }
-        logger.debug("Finished.");
     }
 }
